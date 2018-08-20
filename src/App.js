e9ad86b08d15deecb7faecebc3dc0e13f7a73f0a
jpacsai <judit.pacsai@gmail.com>
2018-08-20 11:50:11 +0200

    feat: add basic content

--- src/App.js
+++ src/App.js
@@ -3,8 +3,37 @@ import React, { Component } from 'react';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-
+      <div  id="drum-machine">
+        <div id="display">
+          <button class='drum-pad' id='q'>Q
+            <audio className='clip' id='Q' src='#'></audio>
+          </button>
+          <button class='drum-pad' id='w'>W
+            <audio className='clip' id='W' src='#'></audio>
+            </button>
+          <button class='drum-pad' id='e'>E
+            <audio className='clip' id='E' src='#'></audio>
+          </button>
+          <button class='drum-pad' id='a'>A
+            <audio className='clip' id='A' src='#'></audio>
+          </button>
+          <button class='drum-pad' id='s'>S
+            <audio className='clip' id='S' src='#'></audio>
+          </button>
+          <button class='drum-pad' id='d'>D
+            <audio className='clip' id='D' src='#'></audio>
+          </button>
+          <button class='drum-pad' id='z'>Z
+            <audio className='clip' id='Z' src='#'></audio>
+          </button>
+          <button class='drum-pad' id='x'>X
+            <audio className='clip' id='X' src='#'></audio>
+          </button>
+          <button class='drum-pad' id='c'>C
+            <audio className='clip' id='C' src='#'></audio>
+          </button>
+          <span id='display-text'></span>
+        </div>
       </div>
     );
   }
