3817ab2680a1fcfc2970396ffa6c927fc5aa1db7
jpacsai <judit.pacsai@gmail.com>
2018-08-20 13:25:01 +0200

    feat: prepare for eventListeners

--- src/App.js
+++ src/App.js
@@ -3,35 +3,46 @@ import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 
 class App extends Component {
+
+  componentWillMount() {
+    //document.addEventListener("click", , false);
+    //document.addEventListener("keyPress", , false);
+  }
+
+  componentWillUnmount() {
+    //document.removeEventListener("click", , false);
+    //document.removeEventListener("keyPress", , false);
+  }
+
   render() {
     return (
       <div  id="drum-machine">
         <div id="display">
-          <button class='drum-pad' id='q'>Q
+          <button className='drum-pad' id='q' >Q
             <audio className='clip' id='Q' src='#'></audio>
           </button>
-          <button class='drum-pad' id='w'>W
+          <button className='drum-pad' id='w'>W
             <audio className='clip' id='W' src='#'></audio>
             </button>
-          <button class='drum-pad' id='e'>E
+          <button className='drum-pad' id='e'>E
             <audio className='clip' id='E' src='#'></audio>
           </button>
-          <button class='drum-pad' id='a'>A
+          <button className='drum-pad' id='a'>A
             <audio className='clip' id='A' src='#'></audio>
           </button>
-          <button class='drum-pad' id='s'>S
+          <button className='drum-pad' id='s'>S
             <audio className='clip' id='S' src='#'></audio>
           </button>
-          <button class='drum-pad' id='d'>D
+          <button className='drum-pad' id='d'>D
             <audio className='clip' id='D' src='#'></audio>
           </button>
-          <button class='drum-pad' id='z'>Z
+          <button className='drum-pad' id='z'>Z
             <audio className='clip' id='Z' src='#'></audio>
           </button>
-          <button class='drum-pad' id='x'>X
+          <button className='drum-pad' id='x'>X
             <audio className='clip' id='X' src='#'></audio>
           </button>
-          <button class='drum-pad' id='c'>C
+          <button className='drum-pad' id='c'>C
             <audio className='clip' id='C' src='#'></audio>
           </button>
           <span id='display-text'></span>
