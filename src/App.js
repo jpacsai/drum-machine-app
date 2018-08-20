7dae8c87c5030a76d9314a1d2bba71f18e3c5939
jpacsai <judit.pacsai@gmail.com>
2018-08-20 23:46:05 +0200

    feat: add sound

--- src/App.js
+++ src/App.js
@@ -67,56 +67,64 @@ class App extends Component {
 						<button
 							className='drum-pad'
 							id='q'
-							onClick={(event) => this.props.clickAction(event) }>Q
-							<audio className='clip' id='Q' src='#'></audio>
+							onClick={(event) => {
+								const sound = document.getElementById(event.target.id.toUpperCase());
+								const s = new Audio (sound.src);
+								console.log(s)
+								s.currentTime = 0;
+								s.play();
+								this.props.clickAction(event);
+								
+							} }>Q
+							<audio className='clip' id='Q' src='https://clyp.it/bgmvqalf?token=e9c60345c6f4b83bc64bd94d8835c9bf' preload="auto"></audio>
 						</button>
 						<button
 							className='drum-pad'
 							id='w'
 							onClick={(event) => this.props.clickAction(event) }>W
-							<audio className='clip' id='W' src='#'></audio>
+							<audio className='clip' id='W' src='https://clyp.it/zilsxnoe'></audio>
 							</button>
 						<button
 							className='drum-pad'
 							id='e'
 							onClick={(event) => this.props.clickAction(event) }>E
-							<audio className='clip' id='E' src='#'></audio>
+							<audio className='clip' id='E' src='https://clyp.it/jbf0m00h'></audio>
 						</button>
 						<button
 							className='drum-pad'
 							id='a'
 							onClick={(event) => this.props.clickAction(event) }>A
-							<audio className='clip' id='A' src='#'></audio>
+							<audio className='clip' id='A' src='https://clyp.it/lb2wp1lr'></audio>
 						</button>
 						<button
 							className='drum-pad'
 							id='s'
 							onClick={(event) => this.props.clickAction(event) }>S
-							<audio className='clip' id='S' src='#'></audio>
+							<audio className='clip' id='S' src='https://clyp.it/4yumiokm'></audio>
 						</button>
 						<button
 							className='drum-pad'
 							id='d'
 							onClick={(event) => this.props.clickAction(event) }>D
-							<audio className='clip' id='D' src='#'></audio>
+							<audio className='clip' id='D' src='https://clyp.it/ksy4wrnw'></audio>
 						</button>
 						<button
 							className='drum-pad'
 							id='z'
 							onClick={(event) => this.props.clickAction(event) }>Z
-							<audio className='clip' id='Z' src='#'></audio>
+							<audio className='clip' id='Z' src='https://clyp.it/hxk50zv0'></audio>
 						</button>
 						<button
 							className='drum-pad'
 							id='x'
 							onClick={(event) => this.props.clickAction(event) }>X
-							<audio className='clip' id='X' src='#'></audio>
+							<audio className='clip' id='X' src='https://clyp.it/z1vofbc1'></audio>
 						</button>
 						<button
 							className='drum-pad'
 							id='c'
 							onClick={(event) => this.props.clickAction(event) }>C
-							<audio className='clip' id='C' src='#'></audio>
+							<audio className='clip' id='C' src='https://clyp.it/dc4qgonn'></audio>
 						</button>
 					</div>
 					<div id='display-text'>{ displayText }</div>
